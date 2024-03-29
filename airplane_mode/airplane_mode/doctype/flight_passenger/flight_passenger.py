e33d71460ac3d21338b1c32c8e7d47c2b9e3f0f0
Hamza-ALsaqaf <hamza1alsaqaf@gmail.com>
2024-03-30 03:22:22 +0530

    feat:a new Read only field Full Name and make it as Title field to the Flight Passenger doctype

--- airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.py
+++ airplane_mode/airplane_mode/doctype/flight_passenger/flight_passenger.py
@@ -1,8 +1,12 @@
 # Copyright (c) 2024, Hamza Alsaqaf and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class FlightPassenger(Document):
-	pass
+	def before_save(self):
+		self.set_full_name()
+		# frappe.errprint(self.full_name)
+	def set_full_name(self):
+		self.full_name=f"{self.first_name} {self.last_name}"
